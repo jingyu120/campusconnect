8502357b51cbfa0a4fb47d066b3e168497682af9
Justin Zhang <jingyu120@gmail.com>
2021-09-19 11:03:06 -0500

    first commit

---
+++ src/main/java/depaul/csc452/group2/campusconnect/CampusconnectApplication.java
@@ -0,0 +1,13 @@
+package depaul.csc452.group2.campusconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depaul/csc452/group2/campusconnect/CampusconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package depaul.csc452.group2.campusconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
