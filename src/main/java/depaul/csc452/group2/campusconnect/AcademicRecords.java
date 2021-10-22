7311ddf7ffe87e88448ac640adf365146b309984
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:03:40 -0700

    Add files via upload

---
+++ src/main/java/depaul/csc452/group2/campusconnect/AcademicRecords.java
@@ -0,0 +1,36 @@
+package depaul.csc452.group2.campusconnect;
+
+import java.sql.Date; 
+import java.sql.Date;
+import java.sql.util.UUID; 
+
+import lombok.Data;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Column; 
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+
+@Table(name = "AcademicRecords")
+@Data
+@Entity
+
+public class AcademicRecords {
+       @Id 
+       private int[] recordID; 
+
+       @Column(name = "courseID")
+       private int[] courseID; 
+
+       @Column(name = "studentID")
+       private long studentID;
+    
+       @Column(name = "courseGrade")
+       private String courseGrade;
+    
+    
+}
