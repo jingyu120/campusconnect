50481ba3c842c33565535907340e75c7f4772cdb
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:36:57 -0700

    Update StudentResourcesRepository.java

--- src/main/java/depaul/csc452/group2/campusconnect/StudentResourcesRepository.java
+++ src/main/java/depaul/csc452/group2/campusconnect/StudentResourcesRepository.java
@@ -2,6 +2,6 @@ package depaul.csc452.group2.campusconnect;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class StudentResourcesRepository extends JpaRepository<StudentResources, long> {
+public class StudentResourcesRepository extends JpaRepository<StudentResources, String> {
     
 }
