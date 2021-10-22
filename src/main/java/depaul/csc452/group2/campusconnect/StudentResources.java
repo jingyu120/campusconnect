d188ee046c7407cf77e9c88028adddf3e29aa8ec
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:04:39 -0700

    Add files via upload

---
+++ src/main/java/depaul/csc452/group2/campusconnect/StudentResources.java
@@ -0,0 +1,32 @@
+package depaul.csc452.group2.campusconnect;
+
+import lombok.Data;
+
+import java.sql.Date;
+import java.sql.Date;
+import java.sql.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.Email;
+
+
+@Data
+@Entity 
+@Table(name = "StudentResources")
+public class StudentResources {
+    @Id 
+    private int[] resourceID;
+
+    @Column(name ="universityNews ")
+    private String universityNews;
+
+    @Column(name = "immunizationStatus")
+    private String immunizationStatus;
+
+      
+    
+}
