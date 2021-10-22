b8ce985701e72d25c51ee04ce67299c29f987388
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:36:32 -0700

    Update AcademicRecordsRepository.java

--- src/main/java/depaul/csc452/group2/campusconnect/AcademicRecordsRepository.java
+++ src/main/java/depaul/csc452/group2/campusconnect/AcademicRecordsRepository.java
@@ -2,6 +2,6 @@ package depaul.csc452.group2.campusconnect;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AcademicRecordsRepository extends JpaRepository<AcademicRecords, long> {
+public interface AcademicRecordsRepository extends JpaRepository<AcademicRecords, String> {
     
 }
