72ceb59d5c9d0aa27f763ab3cc19cb15392b154e
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:04:54 -0700

    Add files via upload

---
+++ src/main/java/depaul/csc452/group2/campusconnect/Tasks.java
@@ -0,0 +1,33 @@
+package depaul.csc452.group2.campusconnect;
+
+
+import lombok.Data;
+
+import java.sql.Date;
+import java.sql.Date;
+import java.sql.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.Email;
+
+@Data
+@Entity 
+@Table(name = "Tasks")
+
+public class Tasks {
+    @Id 
+    private int[] taskID;
+    
+    @Column(name ="userID")
+     private int[] userID;
+
+     @Column(name ="holds")
+     private String holds;
+
+     @Column(name ="toDoList")
+     private String toDoList;
+}
