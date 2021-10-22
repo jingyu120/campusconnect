9803ace97e220af17d4cc9c3e6dcb3cbeb8f996b
Michael Zhou <56005430+Mikeroism@users.noreply.github.com>
2021-10-22 08:13:10 -0700

    Add files via upload

---
+++ src/main/java/depaul/csc452/group2/campusconnect/StudentResourcesRepository.java
@@ -0,0 +1,7 @@
+package depaul.csc452.group2.campusconnect;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class StudentResourcesRepository extends JpaRepository<StudentResources, long> {
+    
+}
