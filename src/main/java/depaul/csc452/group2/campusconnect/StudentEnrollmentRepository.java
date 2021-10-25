140d5baa4a000c8aede54c8976a5295e8505c353
Justin Zhang <jingyu120@gmail.com>
2021-10-25 17:29:25 -0500

    cleaned up examples. deleted class demo code and classes.

--- src/main/java/depaul/csc452/group2/campusconnect/StudentEnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package depaul.csc452.group2.campusconnect;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentEnrollmentRepository extends JpaRepository<StudentEnrollment, StudentCourse> {
-
-}
