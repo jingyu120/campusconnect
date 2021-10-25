7ee03d8d0956f7ca3c136e9ab56e8989fde3f3d3
Justin Zhang <jingyu120@gmail.com>
2021-10-25 16:04:51 -0500

    added dependency for thymeleaf and springweb. created directory structure for frontend and sample code

--- src/main/java/depaul/csc452/group2/campusconnect/frontend/controller/DemoController.java
+++ src/main/java/depaul/csc452/group2/campusconnect/frontend/controller/DemoController.java
@@ -1,9 +1,8 @@
-package depaul.csc452.group2.campusconnect.frontend;
+package depaul.csc452.group2.campusconnect.frontend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
