f7ea77d1283beb79278ebe5fba0d483e51615f02
manlisun <90723170+manlisun@users.noreply.github.com>
2021-11-17 12:46:25 -0600

    add controller class

--- src/main/java/depaul/csc452/group2/campusconnect/CampusConnectApplication.java
+++ src/main/java/depaul/csc452/group2/campusconnect/CampusConnectApplication.java
@@ -1,25 +1,55 @@
 package depaul.csc452.group2.campusconnect;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import depaul.csc452.group2.campusconnect.Student;
+import depaul.csc452.group2.campusconnect.UserProfile;
+import depaul.csc452.group2.campusconnect.UserProfileRepository;
+
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
+@Controller
 public class CampusConnectApplication {
     private static final Logger log = LoggerFactory.getLogger(CampusConnectApplication.class);
 
-    //
+    @Autowired
+    private StudentRepository studentrepository;
+
+    @Autowired
+    private UserProfileRepository userRepository;
+
+    @Autowired
+    private noSQLStudentRepository noSQLStudentRepository;
+
+
+    @GetMapping("/")
+    public String start() {
+        return "homepage";
+    }
+    
+    @GetMapping("/login")
+    public String login() {return "login"; }
+
+    @GetMapping("/home")
+    public String home() {
+        return "homepage";
+    }
+
+
     @Bean
     public CommandLineRunner saveUserProfile(UserProfileRepository repository) {
         return (args) -> {
